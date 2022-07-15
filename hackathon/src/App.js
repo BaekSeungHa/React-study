d887dff3c3997895d3eb48c79205916d238bb74f
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-07-16 01:24:15 +0900

    state

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -2,17 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let post = '대구 라면 맛집';
 
    return (
     <div className="App">
       <div className="black-nav">
-      <h4 style={ {color: 'blue', fontSize: '16px'} }>블로그임</h4>
+      <h4>블로그임</h4>
       </div>
-      <h4>{ post }</h4>
+      <div className="list">
+        <h4>글제목</h4>
+        <p>7월 15일 발행</p>
+      </div>
+      <div className="list">
+        <h4>글제목</h4>
+        <p>7월 16일 발행</p>
+      </div>   
     </div>
   );
 }
 
 export default App;
-     
+//return()안에는 병룔로
+//태그 2개 이상 기입금지
