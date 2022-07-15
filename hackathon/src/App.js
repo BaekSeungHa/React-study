133b1847b9f48aa5c94e75052148d199e9dafdb5
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-07-16 00:56:51 +0900

    2022 new 리액트 2강 : JSX 문법은 3개가 다임

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  let post = '대구 라면 맛집';
+
+   return (
     <div className="App">
-      
+      <div className="black-nav">
+      <h4 style={ {color: 'blue', fontSize: '16px'} }>블로그임</h4>
+      </div>
+      <h4>{ post }</h4>
     </div>
   );
 }
 
 export default App;
+     
