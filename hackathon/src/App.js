9e2a966cd4db356fd80a58eee7dd0cdea8b717b7
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-07-16 15:16:24 +0900

    Destructuring문법

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let post = "대구 라면 맛집";
+  let [글제목, b] = useState('남자 셔츠 추천');
 
-   return (
-    <div className="App">
-      <div className="black-nav">
-      <h4>블로그임</h4>
-      </div>
-      <div className="list">
-        <h4>글제목</h4>
-        <p>7월 15일 발행</p>
-      </div>
-      <div className="list">
-        <h4>글제목</h4>
-        <p>7월 16일 발행</p>
-      </div>   
+  let num = [1, 2, 3];
+  return (
+  <div className="App">
+    <div className="black-nav">
+    <h4>블로그임</h4>
     </div>
-  );
+    <div className="list">
+      <h4>{ 글제목 }</h4>
+      <p>7월 16일 발행</p>
+    </div>   
+  </div>
+);
 }
 
 export default App;
