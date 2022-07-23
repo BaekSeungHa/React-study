1d7af19ade99310aee7f149e06f0a3787d094d07
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-07-23 22:49:08 +0900

    add 가나다순정렬

--- codingapple/src/App.js
+++ codingapple/src/App.js
@@ -4,12 +4,17 @@ import { useState } from "react";
 
 function App() {
   let post = "대구 라면 맛집";
-  let [글제목, 글제목변경] = useState([
+  let [title, setTitle] = useState([
     "남자 코트, 추천",
     "강남 우동맛집",
     "파이썬독학",
   ]);
   let [따봉, 따봉변경] = useState(0);
+
+  function orderTitle() {
+    let newOrder = [...title].sort();
+    setTitle(newOrder);
+  }
 
   return (
     <div className="App">
@@ -17,20 +22,20 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
 
-      <button>가나다순정렬</button>
+      <button onClick={orderTitle}>가나다순으로 정렬하기</button>
 
       <button
         onClick={() => {
-          let copy = [...글제목];
+          let copy = [...title];
           copy[0] = "여자 코트 추천";
-          글제목변경(copy);
+          setTitle(copy);
         }}
       >
         글수정
       </button>
       <div className="list">
         <h4>
-          {글제목[0]}
+          {title[0]}
           <span
             onClick={() => {
               따봉변경(따봉 + 1);
@@ -43,11 +48,11 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목[1]}</h4>
+        <h4>{title[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목[2]}</h4>
+        <h4>{title[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
