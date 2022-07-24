85bef3ac5c6f2c0c095d039b4dcaa47c71b8ced1
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-07-24 22:28:42 +0900

    6.Component

--- codingapple/src/App.js
+++ codingapple/src/App.js
@@ -15,7 +15,7 @@ function App() {
     let newOrder = [...title].sort();
     setTitle(newOrder);
   }
-
+  //함수는 return문 밖에서 사용
   return (
     <div className="App">
       <div className="black-nav">
@@ -47,14 +47,27 @@ function App() {
         </h4>
         <p>2월 17일 발행</p>
       </div>
+
       <div className="list">
         <h4>{title[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
+
       <div className="list">
         <h4>{title[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+      <Modal></Modal>
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
