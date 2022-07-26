63e23fe978e07fa913f1595c7eb2ef521bfabf02
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-07-26 23:39:45 +0900

    컴포넌트

--- react book/src/App.js
+++ react book/src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import MyComponent from "./MyComponent";
 
 function App() {
   return <div className="App"></div>;
 }
+
+const App = () => {
+  return <MyComponent />;
+};
 
 export default App;
