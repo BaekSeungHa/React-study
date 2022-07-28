d1891a9eeda31a363e07d086a56a9ab03868e95e
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-07-29 01:34:37 +0900

    Publishing

--- web-movie-service/src/routes/Home.js
+++ web-movie-service/src/routes/Home.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
-
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
@@ -18,17 +17,23 @@ function Home() {
   }, []);
   return (
     <div>
-      {movies.map((movie) => (
-        <Movie
-          key={movie.id}
-          coverImg={movie.medium_cover_image}
-          title={movie.title}
-          summary={movie.summary}
-          genres={movie.genres}
-        />
-      ))}
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
-
 export default Home;
