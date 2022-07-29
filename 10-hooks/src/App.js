1bf83ac2a03a24607114e25351aa5b7ebc8a7abb
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-07-29 12:25:04 +0900

    add: count file

--- 10-hooks/src/App.js
+++ 10-hooks/src/App.js
@@ -1,8 +1,18 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item - 1);
+  const decrementItem = () => setItem(item - 1);
+  return (
+    <div className="App">
+      <h1>Hello {item}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>Dercrement</button>
+    </div>
+  );
 }
 
 export default App;
