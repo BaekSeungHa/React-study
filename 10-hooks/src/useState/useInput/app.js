cb71aaebc21ed316964e2e0411cf36393a441ee6
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-07-31 20:10:22 +0900

    useInput

---
+++ 10-hooks/src/useState/useInput/app.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { useInput } from "./useInput";
+
+const App = () => {
+  const maxLen = (value) => !value.includes("@");
+  const name = useInput("Mr.", maxLen);
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+
+      <input placeholder="Name" {...name} />
+    </div>
+  );
+};
+
+export default App;
