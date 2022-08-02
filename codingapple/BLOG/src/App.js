d47577a134cfad890668a7af6a64b7197b30e869
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-08-03 01:36:35 +0900

    0803

--- codingapple/BLOG/src/App.js
+++ codingapple/BLOG/src/App.js
@@ -7,7 +7,7 @@ import React, { useState } from "react";
 function App() {
   let post = "대구 라면 맛집";
   let [글제목, 글제목변경] = useState([
-    "남자 코트 추천",
+    "오우 저치기~",
     "외쳐 수찬갓",
     "쵸비우승",
   ]);
@@ -15,12 +15,7 @@ function App() {
   let [modal, setModal] = useState(false);
   let [title, setTitle] = useState(0);
   let [입력값, 입력값변경] = useState("");
-  let today = new Date();
-  let month = today.getMonth() + 1;
-  let date = today.getDate();
-  let day = String(month) + "월" + String(date) + "일";
-
-  let [날짜, 날짜변경] = useState([day, day, day]);
+  let [writeDate, setWriteDate] = useState("");
   return (
     <div className="App">
       <div className="black-nav">
@@ -31,7 +26,7 @@ function App() {
           <div className="list" key={i}>
             <h4
               onClick={() => {
-                setModal(true);
+                setModal(!modal);
                 setTitle(i);
               }}
             >
@@ -61,18 +56,26 @@ function App() {
           </div>
         );
       })}
+      <input
+        onChange={(e) => {
+          입력값변경(e.target.value);
+        }}
+      />
       <button
         onClick={() => {
           let copy = [...글제목];
           입력값 == "" ? null : copy.unshift(입력값);
           글제목변경(copy);
+
           let copycount = [...like];
           입력값 == "" ? null : copycount.unshift(0);
           setlike(copycount);
+
           let copyWriteDate = [...writeDate];
           const now = new Date();
           const month = now.getMonth() + 1;
           const date = now.getDate();
+
           copyWriteDate == ""
             ? null
             : copyWriteDate.unshift(`${month}월 ${date}일`);
@@ -81,29 +84,6 @@ function App() {
       >
         story upload
       </button>
-      <input
-        onChange={(e) => {
-          입력값변경(e.target.value); //state변경함수는 늦게 처리됨
-          console.log(입력값);
-        }}
-      />
-      <button
-        onClick={() => {
-          let copy = [...글제목];
-          copy.unshift(입력값);
-          글제목변경(copy);
-        }}
-      >
-        글발행
-      </button>
-      <button
-        onClick={() => {
-          setModal(!modal);
-        }}
-      >
-        모달 on/off
-      </button>
-
       {modal == true ? <Modal title={title} 글제목={글제목} /> : null}
     </div>
   );
