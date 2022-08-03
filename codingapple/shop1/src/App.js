a128dac507dd6b20d1f28575470e7040279a7f2c
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-08-04 00:16:46 +0900

    setting

--- codingapple/shop1/src/App.js
+++ codingapple/shop1/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { useState } from "react";
+import data from "./data";
 
 function App() {
+  let [shoes] = useState(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="lgiht" variant="light">
+        <Container className="main">
+          <Navbar.Brand href="#home" className="home">
+            ABC Mart
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="main-bg"></div>
+
+      <div className="container">
+        <div className="row">
+          {/* <Card shoes={shoes[0]} i={1}></Card>
+          <Card shoes={shoes[1]} i={2}></Card>
+          <Card shoes={shoes[2]} i={3}></Card> */}
+          {shoes.map((a, i) => {
+            return <Card shoes={shoes[i]} i={i}></Card>;
+          })}
+        </div>
+      </div>
     </div>
   );
+
+  function Card(props) {
+    return (
+      <div className="col-md-4">
+        <img
+          src={
+            "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+          }
+          width="80%"
+        />
+        <h5>{props.shoes.title}</h5>
+        <p>{props.shoes.price}원</p>
+      </div>
+    );
+  }
 }
 
 export default App;
