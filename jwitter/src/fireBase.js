3735efad7cf2986cd544e9affc09295f2c5f2a35
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-08-08 19:06:07 +0900

    add: file

---
+++ jwitter/src/fireBase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBawr9xCzePXrpcag4ddXZO7oF0UaW0qt8",
+  authDomain: "jwitter-ab0a9.firebaseapp.com",
+  projectId: "jwitter-ab0a9",
+  storageBucket: "jwitter-ab0a9.appspot.com",
+  messagingSenderId: "108261627705",
+  appId: "1:108261627705:web:4e4aa431d27f5c15b5bae8",
+};
+const app = initializeApp(firebaseConfig);
